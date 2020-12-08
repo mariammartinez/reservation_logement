c22ada5014e8334d2f6fb40f3ca7687c4f9363e5
mariammartinez <mariammartinez@users.noreply.github.com>
2020-12-08 13:22:36 +0100

    premier test

---
+++ src/Controller/homeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class homeController extends AbstractController{
+    /**
+     * @Route("/hello/{prenom}/age/{age}", name="hello")
+     * @Route("/hello", name="bonjour_solo")
+     *
+     * Montrer la page
+     *
+     * @return void
+     */
+
+    public function hello($prenom = "nada", $age=10){
+        return $this->render(
+            'nuevo.html.twig',
+        [
+            'prenom' => $prenom,
+            'age'=> $age
+        ]
+        );
+    }
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function home(){
+        $jours = ["lundi"=>7, "mardi"=>8, "samedi"=>12];
+
+      return $this->render(
+          'hello.html.twig',
+          ['title' => "buenos dias!",
+              'coco' => 'amigo',
+              'age' => '18',
+              'tableau' => $jours
+          ]
+      );
+    }
+}
+
+?>
