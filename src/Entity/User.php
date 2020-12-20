b6e2fc5146b601486ebe4ad667ad6499b9675a88
mariammartinez <mariammartinez@users.noreply.github.com>
2020-12-20 23:13:45 +0100

    function montant show

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -95,6 +95,11 @@ class User implements UserInterface
     private $bookings;
 
     /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="author", orphanRemoval=true)
+     */
+    private $comments;
+
+    /**
      * permet d'initialiser le slug
      *
      * @ORM\PrePersist
@@ -113,6 +118,7 @@ class User implements UserInterface
         $this->ads = new ArrayCollection();
         $this->userRoles = new ArrayCollection();
         $this->bookings = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -332,6 +338,36 @@ class User implements UserInterface
             // set the owning side to null (unless already changed)
             if ($booking->getBooker() === $this) {
                 $booking->setBooker(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getAuthor() === $this) {
+                $comment->setAuthor(null);
             }
         }
 
